6ced341f7fedd6d078db5a4634ab2efc7fedfff4
ShantoieT57 <107485443+ShantoieT57@users.noreply.github.com>
2022-08-17 11:58:16 +0200

    Update 2. SELECT WHERE ID.sql

--- SQL Queries/2. SELECT WHERE ID.sql
+++ SQL Queries/2. SELECT WHERE ID.sql
@@ -1,6 +1,8 @@
-SELECT id
-      ,name
-      ,price
-      ,quantityInStock
-  FROM dbo.Products
-  WHERE Id = @{$.Parameters.Data.productId}
+--This query will select a specific product record based on it's product ID
+SELECT 
+     id
+     ,name
+    ,price
+    ,quantityInStock
+FROM dbo.Products
+WHERE Id = @{$.Parameters.productId}
